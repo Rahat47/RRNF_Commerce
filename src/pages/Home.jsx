2abac4b7e5e6092bef7edbe8be50a616becd07d2
Rahat47 <djrayhan8@gmail.com>
2021-10-26 23:36:58 +0600

    navbar done

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Announcement from "../components/Announcement";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+    return (
+        <div>
+            <Announcement />
+            {/* Navbar */}
+            <Navbar />
+        </div>
+    );
+};
+
+export default Home;
